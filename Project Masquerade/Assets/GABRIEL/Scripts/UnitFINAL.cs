7d30e96acb60bcd760062f5778e2fe3764ecc885
MUKILTEO\1721783 <1721783@mukilteo.wednet.edu>
2025-04-23 10:12:23 -0700

    enemy ai is good, but this warning is in the way :(

--- Project Masquerade/Assets/GABRIEL/Scripts/UnitFINAL.cs
+++ Project Masquerade/Assets/GABRIEL/Scripts/UnitFINAL.cs
@@ -42,7 +42,7 @@ public class UnitFINAL : MonoBehaviour
         Defense -= actualBlock;
     }
 
-    public void setdefense()
+    public void Setdefense()
     {
         cantakedamage = false;
     }
