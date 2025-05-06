6a9fdc8fe2280302a447012cd33b7890a3deb068
MUKILTEO\1721783 <1721783@mukilteo.wednet.edu>
2025-05-06 08:03:44 -0700

    npc 2

--- Project Masquerade/Assets/ELIAS/Scripts/NPC.cs
+++ Project Masquerade/Assets/ELIAS/Scripts/NPC.cs
@@ -1,16 +1,81 @@
+using System.Collections;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class NPC : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public GameObject dialoguePanel;
+    public TextMesh dialogueTextMesh;
+    public string[] dialogue;
+    private int index;
+
+    public float wordSpeed;
+    public bool playerIsClose;
+
+
+    private void Update()
     {
-        
+        if(Input.GetKeyUp(KeyCode.E) && playerIsClose)
+        {
+            if (dialoguePanel.activeInHierarchy)
+            {
+                zeroText();
+            }
+            else
+            {
+                dialoguePanel.SetActive(true);
+                StartCoroutine(Typing());
+            }
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+
+    public void zeroText()
     {
-        
+        dialogueTextMesh.text = " ";
+        index = 0;
+        dialoguePanel.SetActive(false);
+    }
+
+
+    IEnumerator Typing()
+    {
+        foreach(char letter in dialogue[index].ToCharArray())
+        {
+            dialogueTextMesh.text += letter;    
+            yield return new WaitForSeconds(wordSpeed);
+        }
+    }
+
+    public void NextLine()
+    {
+        if(index < dialogue.Length - 1)
+        {
+            index++;
+            dialogueTextMesh.text = "";
+            StartCoroutine(Typing());
+        }
+        else
+        {
+            zeroText();
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerIsClose = true; 
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerIsClose = false;
+            zeroText();
+        }
     }
 }
